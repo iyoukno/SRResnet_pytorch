6df37c584b88ceff7861bc60899fc79d484eb7a4
iyoukno <609753644@qq.com>
2024-08-29 10:15:24 +0800

    first commit

---
+++ datasets.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/8/28 19:13
+# @Author  : zjt
+# @File    : datasets.py
+# @info    :
+
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import os
+from torchvision import transforms
+from PIL import Image
+
+#图像处理操作，包括随机裁剪，转换张量
+transform = transforms.Compose([transforms.RandomCrop(96),
+                            transforms.ToTensor()])
+
+class PreprocessDataset(Dataset):
+    """预处理数据集类"""
+
+    def __init__(self, imgPath, ex=1):
+        """初始化预处理数据集类"""
+        self.transforms = transform
+
+        for _, _, files in os.walk(imgPath):
+            # ex变量是用于扩充数据集的，在这里默认的是扩充十倍
+            self.imgs = [imgPath + file for file in files] * ex
+
+        np.random.shuffle(self.imgs)  # 随机打乱
+
+    def __len__(self):
+        """获取数据长度"""
+        return len(self.imgs)
+
+    def __getitem__(self, index):
+        """获取数据"""
+        tempImg = self.imgs[index]
+        tempImg = Image.open(tempImg)
+
+        sourceImg = self.transforms(tempImg)  # 对原始图像进行处理
+        cropImg = torch.nn.MaxPool2d(4)(sourceImg)
+        return cropImg, sourceImg
---
+++ train.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/8/28 19:13
+# @Author  : zjt
+# @File    : train.py
+# @info    :
+
+import torch
+from torch.optim import Adam
+from net import SRResNet
+from datasets import PreprocessDataset
+from torch.utils.data import DataLoader
+import tqdm
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class Trainer():
+    def __init__(self, dataset_path, batch):
+        # net
+        self.net = SRResNet()
+
+        # optimizer
+        self.optimizer = Adam(self.net.parameters(), lr=0.01)
+
+        # loss function
+        self.loss_f = torch.nn.MSELoss()
+
+        # datasets dataloader
+        self.datasets = PreprocessDataset(dataset_path)
+        self.dataloader = DataLoader(self.datasets, batch, shuffle=True)
+
+    def train(self, epoch):
+        net = self.net.to(DEVICE)
+        net.train()
+
+        for e in range(epoch):
+            e_loss = 0
+
+            for idx, (corp_img, img) in tqdm(enumerate(self.dataloader)):
+                corp_img, img = corp_img.to(DEVICE), img.to(DEVICE)
+
+                out = net(corp_img)
+                loss = self.loss_f(out, img)
+                e_loss += loss
+
+                # gradient update
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            avg_loss = e_loss / len(self.dataloader)
+            torch.save(net.state_dict(), f'./train_dir/{e}.pth')
+            print(f'model save success, epoch: {e} loss {avg_loss}')
